f37ae09f4211ce4e92fd6bd584ab0d332c74512f
Khagani89 <khaganichh@code.edu.az>
2022-06-07 11:44:38 +0400

    project

---
+++ Task 2/Task 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student
+            {
+                FullName = "Hikmet",
+                GroupNo = "P227",
+               
+            };
+            Student student2 = new Student 
+            {
+               FullName = "Nazim",
+               GroupNo = "P227",
+                           
+            };
+            Student student3 = new Student 
+            {
+            FullName = "Gunel" ,
+            GroupNo = "P227",
+
+            };
+        }
+    }
+}
---
+++ Task 2/Task 2/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_2
+{
+    internal class Student
+    {
+        public Student()
+        {
+            TotalCount++;
+            No = TotalCount;
+        }
+
+        public static int TotalCount;
+
+        public int No { get; }
+        public string FullName;
+        private string _groupNo;
+
+        public string GroupNo
+        {
+            get => _groupNo;
+            set 
+            {
+            if(CheckGroupNo(value))
+             _groupNo = value;
+            }
+
+        }
+        public bool CheckGroupNo(string groupNo)
+        {
+            if (!string.IsNullOrWhiteSpace(groupNo) && GroupNo.Length == 4)
+            {
+                if (char.IsLetter(groupNo[0]))
+                    for (int i = 1; i < groupNo.Length; i++)
+                    {
+                        if (!char.IsDigit(groupNo[i]))
+                            return false;
+                    }
+                return true;
+            }
+             return false;
+        }
+           
+    }
+}
